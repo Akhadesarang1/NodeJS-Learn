939f8e394dc6bf99bc67c1f56580e3f7358cacda
Akhadesarang1 <akhadesarang1@gmail.com>
2026-02-03 12:07:34 +0530

    Add first Server code

--- Day01/NODE-FIRST-APP/JavaScript-Summary/array.js
+++
@@ -1,8 +0,0 @@
-// const hobbies = ['Gym','Cooking'];
-// for (let hobby of hobbies) {
-//     console.log('My hobbies are: '+hobby);
-// }
-
-const hobbies = ['Gym','Cooking'];
-console.log(hobbies.map(hobby => 'Hobby: ' + hobby));
-console.log(hobbies);
--- Day01/NODE-FIRST-APP/JavaScript-Summary/arrowfunction.js
+++
@@ -1,5 +0,0 @@
-// const add = (a, b ) =>a+b;
-// console.log(add(1,2));
-
-const addOne = a => a+1;
-console.log(addOne(4));
