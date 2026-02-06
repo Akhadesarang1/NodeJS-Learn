bc08a6bf13a3e05edb1043e0dc6d7c1dfc85b484
Akhadesarang1 <akhadesarang1@gmail.com>
2026-02-06 15:59:23 +0530

    Add code files for routing

--- Day04/Server.js
+++ Day04/Server.js
@@ -1,63 +1,10 @@
 const http = require('http');
-const fs = require('fs');
 
-const server = http.createServer((req, res) => {
+const routes = require('./routes.js');
 
-    const url = req.url;
-    const method = req.method;
+// const server = http.createServer(routes);
+console.log(routes.someText);
 
-    // HOME ROUTE
-    if (url === '/' && method === 'GET') {
-
-        res.setHeader('Content-Type', 'text/html');
-        res.write('<html>');
-        res.write('<head><title>Enter message</title></head>');
-        res.write(
-            '<body>' +
-                '<form action="/message" method="POST">' +
-                    '<input type="text" name="message">' +
-                    '<button type="submit">Send</button>' +
-                '</form>' +
-            '</body>'
-        );
-        res.write('</html>');
-        return res.end(); // stop execution
-    }
-
-    // MESSAGE ROUTE
-    if (url === '/message' && method === 'POST') {
-
-        const body = [];
-
-        req.on('data', (chunk) => {
-            console.log(chunk);
-            body.push(chunk);
-        });
-
-        req.on('end', () => {
-
-            const parsedBody = Buffer.concat(body).toString();
-            const message = parsedBody.split('=')[1];
-
-            fs.writeFile('message.txt', message, err => {
-
-                res.statusCode = 302;
-                res.setHeader('Location', '/');
-                return res.end(); // response sent once
-            });
-
-        });
-
-        return; // VERY IMPORTANT → prevent double response
-    }
-
-    // DEFAULT RESPONSE
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html>');
-    res.write('<head><title>My First Page</title></head>');
-    res.write('<body><h1>Hello from my node.js server</h1></body>');
-    res.write('</html>');
-    res.end();
-});
+const server = http.createServer(routes.handler);
 
 server.listen(3000);
