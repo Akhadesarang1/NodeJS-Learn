ef7dea19ac652bd90fce006b83857abcdb73716b
Sarang Akhade <Akhadesarang1@gmail.com>
2026-02-27 12:52:55 +0530

    feat: configure Sequelize connection for MySQL database

--- Day17/notes.md
+++ Day17/notes.md
@@ -1,2 +1,4 @@
 Day17
-1. 
+1. Understood basic of Sequelize and how it work bts also understood - Models, Instances , Queries , Associations
+2. mysql2 and sequelize are needed to install together
+3. Configured Sequelize instance to connect Node.js application with MySQL database using database name, username, password, dialect, and host.
--- Day17/util/database.js
+++ Day17/util/database.js
@@ -1,10 +1,8 @@
-const mysql = require('mysql2');
+const Sequelize = require('sequelize');
 
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'nodejs-learn',
-    password: 'sAr@ng.akhade123'
+const sequelize = new Sequelize('nodejs-learn','root','sAr@ng.akhade123', {
+    dialect: 'mysql',
+    host: 'localhost'
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
