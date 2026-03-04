224efa8de6e321cacbc7d90ab41b40e6a6bbebe6
Sarang Akhade <Akhadesarang1@gmail.com>
2026-03-04 13:45:45 +0530

    feat: add dummy user initialization and attach user to request middleware

--- Day19/Server.js
+++ Day19/Server.js
@@ -15,18 +15,19 @@ app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
-
-// data retrieving
-// db.execute('SELECT * FROM products') 
-//   .then(result => { 
-//     console.log(result[0], result[1]); 
-//   }) 
-//   .catch(err => { 
-//     console.log(err); 
-//   }); 
+const { name } = require('ejs');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req,res,next) => {
+  User.findByPk(1)
+  .then(user => {
+    req.user = user;
+    next();
+  })
+  .catch(err => console.log(err));
+});
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -37,9 +38,20 @@ Product.belongsTo(User, {constraints: true, onDelete: 'CASCADE'});
 User.hasMany(Product);
 
 sequelize
-  .sync({force: true})
+  // .sync({force: true})
+  .sync()
   .then(result => {
-    console.log(result);
+    return User.findByPk(1);
+    // console.log(result);
+  })
+  .then(user => {
+    if (!user) {
+      return User.create({ name: 'Sarang', email: 'akhadesarang1@gmail.com'});
+    }
+    return user;
+  })
+  .then(user => {
+    console.log(user);
     app.listen(3000);
   })
   .catch(err => {
--- Day19/notes.md
+++ Day19/notes.md
@@ -1,4 +1,5 @@
 Day19
 1. Defined a User model in Sequelize with fields id (primary key, auto-increment), name, and email to represent the users table in the database.
 2. Created a one-to-many association between User and Product using Sequelize where a user can have many products and each product belongs to a single user, with cascade deletion enabled.
-3. 
+3. Implemented a dummy user setup by checking if a user exists in the database after Sequelize sync, creating one if not present, and attaching the user to every request using middleware.
+4. 
